181c71b3175070d43621fff8308c847900f3a1b8
deathcap <deathcap@gmx.co.uk>
2014-02-04 21:37:46 -0800

    Store Blob URL in localStorage to help avoid URL mismatch in SharedWorker

--- index.js
+++ index.js
@@ -3,15 +3,42 @@ var EventEmitter = require('events').EventEmitter;
 var inherits = require('inherits');
 var path = require('path');
 
+var name = 'rtc-signaller-sw';
+
 module.exports = function(opts) {
   return new Messenger(opts);
 };
 
+var isValidBlobURL = function(url, cb) {
+  var fr = new FileReader(url);
+  fr.onloadend = function() {
+    var valid = fr.result && fr.result.length !== 0;
+    cb(url, valid);
+  };
+
+  fr.onerror = function() {
+    cb(url, false);
+  }
+};
+
 function Messenger(opts) {
-  var url = 'data:text/javascript,' + require('./worker.js');
+  var text = require('./worker.js');
+  console.log(text);
+
+  var blob = new Blob([text], {type: 'text/javascript'});
+
+  var url = window.localStorage[name];
+  if (!url) {
+    // save Blob URL across instances since must match
+    window.localStorage[name] = url = URL.createObjectURL(blob);
+  }
   console.log(url);
 
-  this.worker = new SharedWorker(url, 'rtc-signaller-sw');
+  try {
+    this.worker = new SharedWorker(url, 'rtc-signaller-sw');
+  } catch (e) {
+    console.log('FAIL',e);
+  }
 
   var self = this;
   this.worker.port.addEventListener('message', function(ev) {
