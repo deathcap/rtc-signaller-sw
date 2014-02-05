6b2a2bb716b387a0da2ee6880dca34d87a37694b
deathcap <deathcap@gmx.co.uk>
2014-02-04 22:48:08 -0800

    Update test.js to send only after open

--- test.js
+++ test.js
@@ -8,14 +8,16 @@ console.log('We are '+id);
 messenger.on('data', function(ev) {
   console.log(ev);
 });
-var count = 0;
-var interval = setInterval(function() {
-  messenger.write('hi from '+id);
-  /* test close()
-  ++count;
-  if (count > +location.hash.substr(1)) {
-    messenger.close();
-    clearInterval(interval);
-  }
-  */
-}, 1000);
+messeger.on('open', function() {
+  var count = 0;
+  var interval = setInterval(function() {
+    messenger.write('hi from '+id);
+    /* test close()
+    ++count;
+    if (count > +location.hash.substr(1)) {
+      messenger.close();
+      clearInterval(interval);
+    }
+    */
+  }, 1000);
+});
