43d9b6471ac73b96a09ae7d0f58a298b90914fac
deathcap <deathcap@gmx.co.uk>
2014-02-04 21:17:43 -0800

    Add __dirname to SharedWorker URL

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 'use strict';
 var EventEmitter = require('events').EventEmitter;
 var inherits = require('inherits');
+var path = require('path');
 
 module.exports = function(opts) {
   return new Messenger(opts);
 };
 
 function Messenger(opts) {
-  this.worker = new SharedWorker('worker.js', 'rtc-signaller-sw');
+  this.worker = new SharedWorker(path.join(__dirname, 'worker.js'), 'rtc-signaller-sw');
 
   var self = this;
   this.worker.port.addEventListener('message', function(ev) {
