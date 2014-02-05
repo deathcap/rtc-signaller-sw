318d6e962689564debd67cb8f0be092a755a1cd1
deathcap <deathcap@gmx.co.uk>
2014-02-04 22:00:54 -0800

    Remove count from test

--- test.js
+++ test.js
@@ -8,9 +8,11 @@ messenger.on('data', function(ev) {
 var count = 0;
 var interval = setInterval(function() {
   messenger.write('hi from '+location.hash);
+  /*
   ++count;
   if (count > +location.hash.substr(1)) {
     messenger.close();
     clearInterval(interval);
   }
+  */
 }, 1000);
