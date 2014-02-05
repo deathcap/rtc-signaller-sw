e487872683ff31b450bb058f663f64fc8ca62d77
deathcap <deathcap@gmx.co.uk>
2014-02-04 22:03:39 -0800

    Show random ID in test example

--- test.js
+++ test.js
@@ -2,13 +2,16 @@
 
 var messenger = require('./')();
 
+var id = 'instance-'+Math.random(); // for example only
+console.log('We are '+id);
+
 messenger.on('data', function(ev) {
   console.log(ev);
 });
 var count = 0;
 var interval = setInterval(function() {
-  messenger.write('hi from '+location.hash);
-  /*
+  messenger.write('hi from '+id);
+  /* test close()
   ++count;
   if (count > +location.hash.substr(1)) {
     messenger.close();
