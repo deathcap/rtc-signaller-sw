c98550828c311d35a6d1c4aae3e1b2eeec047ecc
deathcap <deathcap@gmx.co.uk>
2014-02-04 21:43:27 -0800

    Embed an ugly multiline literal for the worker script

--- index.js
+++ index.js
@@ -21,12 +21,40 @@ var isValidBlobURL = function(url, cb) {
   }
 };
 
+window.isValidBlobURL = isValidBlobURL;
+
 function Messenger(opts) {
-  var text = require('./worker.js');
+  var text = [
+'var ports = [];',
+'',
+'self.addEventListener("connect", function(connectEvent) {',
+'  var newPort = connectEvent.ports[0]; /* note: always exactly one port */',
+'',
+'  newPort.onmessage = function(messageEvent) {',
+'    if (messageEvent.data === null) {',
+'      ports.splice(ports.indexOf(newPort), 1);',
+'      return;',
+'    };',
+'',
+'    newPort.postMessage("replying to "+ports.length+" connections");',
+'',
+'    for (var i = 0; i < ports.length; ++i) {',
+'      var port = ports[i];',
+'',
+'      if (port !== newPort) /* send to everyone but ourselves */',
+'        port.postMessage(messageEvent.data);',
+'    }',
+'  };',
+'',
+'  ports.push(newPort);',
+'',
+'  //newPort.postMessage("welcome, connection #"+ports.length);',
+'});'].join('\n');
   console.log(text);
 
   var blob = new Blob([text], {type: 'text/javascript'});
 
+  //delete window.localStorage[name];
   var url = window.localStorage[name];
   if (!url) {
     // save Blob URL across instances since must match
